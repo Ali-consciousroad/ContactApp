142ab0c254ac3405155df802194233eb949dc14a
Ali-consciousroad <ali_dindar@live.be>
2021-05-14 13:27:00 +0200

    Render default contact list

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,10 +5,18 @@ class ListContacts extends Component {
     // render the UI of our component
     render() {
         // Show the this.props object inside the console
-        console.log('Props', this.props)
+        // console.log('Props', this.props)
         return (
             <ol className='contact-list'>
-                <li>Hello world</li>
+                {this.props.contacts.map((contact) => (
+                    <div>
+                        <li>Hello world</li>
+                        {/* This allows us to update only the specific element and not all the list */}
+                        <li key={contact.id}>
+                        {contact.name}
+                        </li>
+                    </div>
+                ))}
             </ol>
         )
     }
